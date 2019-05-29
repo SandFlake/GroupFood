1cfa93782b4ed4890b3498708da645610558fd09
Sara Dalvig <dalvigsara@gmail.com>
2019-05-29 09:42:09 +0200

    tried API fetching with ajax

--- like_button.js
+++ like_button.js
@@ -24,3 +24,59 @@ class LikeButton extends React.Component {
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
+
+// Fetching from API //
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    users: [],
+    error: null
+  }
+
+  fetchUsers() {
+  // Where we're fetching data from
+  fetch(`https://jsonplaceholder.typicode.com/users`)
+    // We get the API response and receive data in JSON format...
+    .then(response => response.json())
+    // ...then we update the users state
+    .then(data =>
+      this.setState({
+        users: data,
+        isLoading: false,
+      })
+    )
+    // Catch any errors we hit and update the app
+    .catch(error => this.setState({ error, isLoading: false }));
+}
+
+render() {
+  const { isLoading, users, error } = this.state;
+  return (
+    <React.Fragment>
+      <h1>Random User</h1>
+      // Display a message if we encounter an error
+      {error ? <p>{error.message}</p> : null}
+      // Here's our data check
+      {!isLoading ? (
+        users.map(user => {
+          const { username, name, email } = user;
+          return (
+            <div key={username}>
+              <p>Name: {name}</p>
+              <p>Email Address: {email}</p>
+              <hr />
+            </div>
+          );
+        })
+      // If there is a delay in data, let's let the user know it's loading
+      ) : (
+        <h3>Loading...</h3>
+      )}
+    </React.Fragment>
+  );
+}
+
+componentDidMount() {
+  this.fetchUsers();
+}
--- scriptBeer.js
+++ scriptBeer.js
@@ -3,7 +3,8 @@ $("#ingredient1").on("keyup", function() {
 
    var ing1 = $("#ingredient1").val();
    console.log(ing1);
-   var urlString = "https://www.food2fork.com/api/search?key=69810c988c6c70e14035a686640d095d&q="+ing1;
+
+   var urlString2 = "https://api.punkapi.com/v2/beers"+ing1;
    $.ajax({
        url: urlString,
        dataType: "JSON"
@@ -13,15 +14,13 @@ $("#ingredient1").on("keyup", function() {
        for (i = 0; i < data.recipes.length; i++) {
 
             var title = data.recipes[i].title;
-            var source = data.recipes[i].source_url;
-            var imgSrc = data.recipes[i].image_url;
-            var publisher = data.recipes[i].publisher;
 
-            console.log(data.recipes[3].title + " " + data.recipes[3].source + " " + data.recipes[i].imgSrc);
+
+            console.log(data.recipes[3].title);
             var listClasses = "row titleAlert alert alert-dark text-light";
            console.log("Recipes found" + data.recipes.length);
            $("#recipe-list").append(
-               '<li class="'+listClasses+'">'+generateSpanCode(imgSrc, title, source, i)+'</li>'
+               '<li class="'+listClasses+'">'+generateSpanCode(imgSrc, title, year, i)+'</li>'
            );
        }
    }).fail(function(data) {
@@ -30,12 +29,12 @@ $("#ingredient1").on("keyup", function() {
 }
 });
 
-function generateSpanCode(imgSrc, title, source, i){
+function generateSpanCode(imgSrc, title, year, i){
    var imgClasses = "col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4";
    var textClasses = "col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 vertical-center";
    var code = '<span class="'+imgClasses+'"> <img src="'+imgSrc+'" alt="PosterImage"> </span>'+
    '<span class="'+textClasses+'"> <strong>Title '+(i+1)+':</strong>'+title+'</span>'+
-   '<span class="'+textClasses+'"> <strong>Source:</strong>'+source+'</span>';
+   '<span class="'+textClasses+'"> <strong>Year:</strong>'+year+'</span>';
 
    return code;
 }
